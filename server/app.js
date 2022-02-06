7364edc3a5bf54ea4c0dfa8220b5b43d623f827a
HongInhye <hinhyu@naver.com>
2022-02-06 22:59:21 +0900

    linked database

--- server/app.js
+++ server/app.js
@@ -2,7 +2,11 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// var logger = require('morgan');
+var morgan = require('morgan');
+
+/*시퀼라이즈를 통해 익스프레스 앱과 MySQL연결*/
+const { sequelize } = require('./models');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,10 +14,21 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+//app.set('views', path.join(__dirname, 'views'));
+app.set('port', process.env.PORT || 3001);
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+//db.sequelize를 불러와서 sync 메서드를 사용해 서버 실행 시 MySQL과 연동
+sequelize.sync({ force: false }) //true로 설정하면 서버 실행 시 마다 테이블을 재생성. 테이블을 잘못 만든 경우에 true로 설정하면 됨
+  .then(() => {
+    console.log('데이터베이스 연결 성공');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.use(morgan('dev'));
+//app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -36,6 +51,10 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기 중');
 });
 
 module.exports = app;

c907952b9b3db67b48035335ecd7c8d262dcc5ca
namseohyeon <syn5421a@gamil.com>
2022-03-24 04:30:59 +0900

    image upload

--- server/index.js
+++ server/index.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const bodyParser = require('body-Parser');
 
 app.use(express.json());
 app.use(cors());
-
 const db = require('./models');
-
 //라우터
 const studyRouter = require('./routes/Study');
 const usersRouter = require('./routes/Users');
 
 app.use("/study", studyRouter);
 app.use("/auth", usersRouter);
-
+// app.use(bodyParser.urlencoded({extended: false}));
+// app.use(bodyParser.json())
+app.use(cors())
 db.sequelize.sync().then(() => {
     app.listen(3001, () => {
         console.log("Server Running on Port 3001");
